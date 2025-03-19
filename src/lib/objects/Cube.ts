c206c6968f24e3ddfd88dda38cbff143a5abe508
aaandissu <andissu.dd@gmail.com>
2025-03-18 23:23:54 -0300

    feat(texture): adding texture to the cube mat

--- src/lib/objects/Cube.ts
+++ src/lib/objects/Cube.ts
@@ -1,4 +1,12 @@
-import { BoxGeometry, Mesh, MeshStandardMaterial, MathUtils } from "three";
+import {
+  BoxGeometry,
+  Mesh,
+  MeshStandardMaterial,
+  TextureLoader,
+  MathUtils,
+} from "three";
+import uvTexture from "@/assets/textures/uv-test-col.png";
+import bwTexture from "@/assets/textures/uv-test-bw.png";
 
 interface Tickable {
   tick: (delta: number) => void;
@@ -8,12 +16,25 @@ type TickableMesh = Mesh<BoxGeometry, MeshStandardMaterial> & Tickable;
 
 const radiansPerSec = MathUtils.degToRad(30);
 
+function createMaterial() {
+  const textureLoader = new TextureLoader();
+
+  const texture = textureLoader.load(uvTexture);
+
+  const material = new MeshStandardMaterial({
+    color: "blue",
+    map: texture,
+    roughnessMap: texture,
+  });
+  return material;
+}
+
 function createCube() {
   // create a geometry
   const geometry = new BoxGeometry(1, 1, 1);
 
   // create a default (white) Basic material
-  const material = new MeshStandardMaterial({ color: "gray" });
+  const material = createMaterial();
 
   const cube = new Mesh(geometry, material) as TickableMesh;
   cube.rotation.set(-0.5, -0.1, 0.8);
